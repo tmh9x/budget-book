c75d8506a4dfb5974a413844669d116b5e1fe4c2
onben <ons.benyoussef12@outlook.com>
2024-02-14 12:17:42 +0100

    add prisma module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PaymentService } from './payment/payment.service';
 import { PaymentController } from './payment/payment.controller';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PrismaModule],
   controllers: [AppController, PaymentController],
   providers: [AppService, PaymentService],
 })
