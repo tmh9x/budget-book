2a7370b9e7576134e4dcbea6b55ef867c5bac3d1
Mone19 <fuocoangelos@yahoo.de>
2024-02-29 15:21:40 +0100

    runner test

--- frontend/src/environments/environment.dev.ts
+++ frontend/src/environments/environment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api/payment',
+  production: true,
+  apiUrl: 'http://budget-book.byte-jumper.de/api/payment',
 };
