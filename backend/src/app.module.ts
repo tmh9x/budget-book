bd2fb9f9ee53298df53bda5cefa6b130b33b6149
Mone19 <fuocoangelos@yahoo.de>
2024-03-04 17:30:50 +0100

    database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -20,7 +20,7 @@ import { ConfigModule } from '@nestjs/config';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       autoLoadEntities: true,
-      synchronize: false, // wird auf false gesetzt, wenn wird das deployen werden
+      synchronize: true,
     }),
     AuthModule,
   ],
