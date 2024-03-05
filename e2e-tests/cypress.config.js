7052d9e6b882f46b2ad7f56f9f58949422153b76
onben <ons.benyoussef12@outlook.com>
2024-03-05 11:46:32 +0100

    cypress code anpassen

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   video: true,
   videoCompression: true,
   env: {
-    jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImVtYWlsIjoib25zLmJlbi15b3Vzc2VmQGRvY2MudGVjaHN0YXJ0ZXIuZGUiLCJpYXQiOjE3MDk1NjExMzgsImV4cCI6MTcwOTgyMDMzOH0.zerdTnvSpxs5lRQ0CrsHu5iXsI268xCirkVLHWih5WU'
+    jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImVtYWlsIjoib25zLmJlbi15b3Vzc2VmQGRvY2MudGVjaHN0YXJ0ZXIuZGUiLCJpYXQiOjE3MDk2MzA1MTQsImV4cCI6MTcwOTg4OTcxNH0.V8zK0z42qCGxJAT4qppteFbfseSY9L_PdCpZ5rk90bc'
   },
   e2e: {
     setupNodeEvents(on, config) {
